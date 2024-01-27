b7a1c3eb1134cf6b646b12d9cb89ecd4373f70a2
Konstantin Makarchev <kostya27@gmail.com>
2024-01-27 23:28:21 +0300

    fix

--- plot_history.py
+++ plot_history.py
@@ -10,7 +10,7 @@ with open("history.yml", "r") as stream:
     except yaml.YAMLError as exc:
         print(exc)
 
-fig, axs = plt.subplots(2, figsize=(10, 10))
+fig, axs = plt.subplots(3, figsize=(10, 15))
 
 versions = sorted(y.keys(), key=lambda v: version.parse(v))
 modes = []
@@ -23,29 +23,41 @@ plt.rcParams['image.cmap']='jet'
 
 axs[0].grid(c='0.9')
 axs[0].set_axisbelow(True)
-axs[0].set_title('Run time, s', fontstyle='italic')
+axs[0].set_title('Run time, s (lower better)', fontstyle='italic')
 
 axs[1].grid(c='0.9')
 axs[1].set_axisbelow(True)
-axs[1].set_title('Incremental compile time, s', fontstyle='italic')
+axs[1].set_title('Incremental compile time, s (lower better)', fontstyle='italic')
+
+axs[2].grid(c='0.9')
+axs[2].set_axisbelow(True)
+axs[2].set_title('Coefficient: run time divided by release time, 0..1 (higher better)', fontstyle='italic')
 
 for mode in modes:
     keys = []
     times = []
     comp_time = []
     comp_time2 = []
+    cf = []
     for version in versions:
         if mode in y[version]:
             keys.append(version)
             times.append(y[version][mode]['time'])
             comp_time.append(y[version][mode]['compile1'])
             comp_time2.append(y[version][mode]['compile2'])
+            cf.append(y[version][mode]['cf'])
     axs[0].plot(keys, times)
     axs[0].scatter(keys, times, label=mode, s=100, marker='D')
+
     axs[1].plot(keys, comp_time2)
     axs[1].scatter(keys, comp_time2, label=mode, s=100, marker='D')
+
+    if "release" not in mode:
+        axs[2].plot(keys, cf)
+        axs[2].scatter(keys, cf, label=mode, s=100, marker='D')
 
 plt.grid(color='0.95')
 axs[0].legend(loc='center left')
 axs[1].legend(loc='center left')
+axs[2].legend(loc='center left')
 plt.savefig('releases.png')
